5ab81338dc0a1dec9621f5d5a908ebcdbcc9fb22
Tsholofelo Nkuna <tnkuna@hartic.com>
2024-05-07 14:01:11 -0500

    Migrations to remove MemoryRecallTable

---
+++ Calculator.SQLServer.DAL/Migrations/20240507190021_RemoveMemoryRecallTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calculator.SQLServer.DAL.Migrations
+{
+    public partial class RemoveMemoryRecallTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MemoryRecall");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MemoryRecall",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Inactive = table.Column<bool>(type: "bit", nullable: false),
+                    MRValue = table.Column<double>(type: "float", nullable: false),
+                    MasterId = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemoryRecall", x => x.Id);
+                });
+        }
+    }
+}
